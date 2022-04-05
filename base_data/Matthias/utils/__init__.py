9740d42053a2ccc1d3b7cb9e33739789ec18a11b
MDroth <38705818+MRD-Git@users.noreply.github.com>
2022-04-05 19:09:32 +0200

    conf_matrix_dev

---
+++ base_data/Matthias/utils/__init__.py
@@ -0,0 +1,6 @@
+"""
+Apparently, the mere existence of this "__init__.py" file within the folder "utils" allows us to import "utils".
+
+[1] https://docs.python.org/3/tutorial/modules.html
+[2] https://stackoverflow.com/questions/4383571/importing-files-from-different-folder
+"""
